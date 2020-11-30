1a5d9dbd0cdec97a72ca952d5082fe46c8df20da
salubrious-reference-9997 <sgd3@cornell.edu>
2020-11-30 18:42:18 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -25,7 +25,11 @@ end
   array[index] = "X"
 end
 
-def position_taken?(board, index)
-  input_to_index == "   "
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
 end
   # code your #position_taken? method here!
