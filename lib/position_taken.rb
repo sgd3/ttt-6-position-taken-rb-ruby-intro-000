d0f3f4c524afa26e0fd75def91d81afc0ee1d4c0
salubrious-reference-9997 <sgd3@cornell.edu>
2020-11-30 18:37:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,31 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i-1
+end
+
+def update_array_at_with(board, index, character = "X")
+  board[index] = "X"
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+  return board
+end
+
+  def update_array_at_with(array, index, character = "X")
+  array[index] = "X"
+end
+
 def position_taken?(board, index)
-  array[5]
-  false
+  input_to_index == "   "
 end
   # code your #position_taken? method here!
